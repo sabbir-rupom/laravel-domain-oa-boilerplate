54e06c038d0a44ead2a1b9aec9c2496d12d03b61
Sabbir Hossain <35090483+sabbir-rupom@users.noreply.github.com>
2022-06-21 00:06:43 +0600

    Product domain example moved to example-1 branch

--- app/Domains/Product/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Domains\Product\Http\Controllers\IndexController;
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('product')->group(function () {
-    Route::redirect('/', '/list', 301);
-    Route::get('/list', [IndexController::class, 'index'])->name('product.list');
-});
